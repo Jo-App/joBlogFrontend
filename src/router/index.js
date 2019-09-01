f5cc2d0b3f65c3135ac0a6179141140f706e34f2
조성현 <>
2019-09-01 20:55:26 +0900

    라우터 user admin 분리

--- src/router/index.js
+++ src/router/index.js
@@ -2,79 +2,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Header from '../components/common/Header.vue';
-import Footer from '../components/common/Footer.vue';
-import Index from '../components/Index.vue';
-import Contact from '../components/Contact.vue';
-
-import Test from '../components/Test.vue';
-import test2 from '../components/test2.vue';
-import test3 from '../components/test3.vue';
-import DevBoardList from '../components/DevBoardList.vue';
-
-
+import user from './user.js';
+import admin from './admin.js';
 
 Vue.use(VueRouter);
 
-const routes = [
-      {  
-        path: '/', 
-        redirect:'/index'
-      },
-      {
-        path: '/index',
-        name: 'index',
-        components:{
-          header: Header,
-          content: Index,
-          footer: Footer
-        },
-      },
-      {
-        path: '/contact',
-        name: 'contact',
-        components:{
-          header: Header,
-          content: Contact,
-          footer: Footer
-        }
-      },
-      {
-        path: '/test',
-        name: 'test',
-        components:{
-          header: Header,
-          content: Test,
-          footer: Footer,
-        },
-        meta: { authRequired : true }
-      },
-      {
-        path: '/test2',
-        name: 'test2',
-        components:{
-          content: test2,
-        },
-      },
-      {
-        path: '/test3',
-        name: 'test3',
-        components:{
-          content: test3,
-        },
-      },
-      {
-        path: '/devBoardList',
-        name: 'devBoardList',
-        components:{
-          content: DevBoardList,
-        },
-      }
-    ]
+const routes = [...user, ...admin]
 
-  export const router = new VueRouter({
-    //mode: 'history',
-    routes
-    // get rid of #
-  });
+export const router = new VueRouter({
+  //mode: 'history',
+  routes
+  // get rid of #
+});
   
